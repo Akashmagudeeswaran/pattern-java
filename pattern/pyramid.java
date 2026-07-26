50ea65414a427238d6787341dc81d1142abc537a
AKASH M <akashmagu12@gmail.com>
2026-07-26 18:33:14 +0530

    Update pyramid.java

--- pattern/pyramid.java
+++ pattern/pyramid.java
@@ -5,7 +5,7 @@ public class pyramid {
         for(int i=1;i<=n;i++){
             for(int s=1;s<=n-i;s++){
                 System.out.print(" ");
-            }
+            } 
             for(int j=1;j<=i;j++){
                 System.out.print("* ");
             }
