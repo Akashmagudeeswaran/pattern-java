6780c9ea2b6ebe922e4855a40342f8de98cb6930
AKASH M <akashmagu12@gmail.com>
2026-07-29 21:20:32 +0530

    Update diamond.java

--- pattern/diamond.java
+++ pattern/diamond.java
@@ -6,7 +6,7 @@ public class diamond {
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= (2 * i - 1); j++) {
+            for (int j = 1; j <= (2 * i - 1); j++) { 
                 System.out.print("*");
             }
             System.out.println();
